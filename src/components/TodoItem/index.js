39fef3e6402319316f98b2495f825cb717882db4
PruthviAsadi <pruthvi.asadi@gmail.com>
2024-09-12 11:13:33 +0530

    enhanced simple todos

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,17 +1,69 @@
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
-  const onDelete = () => {
+  const {deleteTodo, todoList, onEditedTask, toggleTaskCompletion} = props
+  const {id, title, isChecked} = todoList
+  const [isEdit, setIsEdit] = useState(false)
+  const [taskTitle, setTaskTitle] = useState(title)
+
+  const onDeleteTodo = () => {
     deleteTodo(id)
   }
+
+  const onClickEdit = () => {
+    setIsEdit(true)
+  }
+
+  const onSaveEditTask = () => {
+    setIsEdit(false)
+    onEditedTask(id, taskTitle)
+  }
+
+  const onChangeTitle = event => {
+    setTaskTitle(event.target.value)
+  }
+
+  const onClickCheckInput = event => {
+    toggleTaskCompletion(id)
+  }
+
+  const isCheckedClass = isChecked ? 'checked-title' : ''
+
   return (
-    <li className="user-card-container">
-      <div className="user-details-container">
-        <p className="user-designation"> {title} </p>
-      </div>
-      <button className="delete-button" onClick={onDelete}>
+    <li className="card">
+      {isEdit ? (
+        <div className="text-btn-div">
+          <input
+            className="edit-input"
+            type="text"
+            value={taskTitle}
+            onChange={onChangeTitle}
+          />
+          <button className="save-edit-btn" onClick={onSaveEditTask}>
+            Save
+          </button>
+        </div>
+      ) : (
+        <div className="text-btn-div">
+          <div className="input-label-div">
+            <input
+              className="check-box"
+              checked={isChecked}
+              onChange={onClickCheckInput}
+              type="checkbox"
+              id={id}
+            />
+            <p htmlFor={id} className={`title ${isCheckedClass}`}>
+              {title}
+            </p>
+          </div>
+          <button className="save-edit-btn" onClick={onClickEdit}>
+            Edit
+          </button>
+        </div>
+      )}
+      <button onClick={onDeleteTodo} className="delete-btn">
         Delete
       </button>
     </li>
